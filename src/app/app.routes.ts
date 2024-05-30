6d7de90da5ac8745fdbaf63d5c0f5204fedaf1fc
Moon <ann.zingan@gmail.com>
2024-05-30 15:34:35 +0300

    non-auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,12 @@ import { IndexComponent } from './common/index/index.component';
 import { PostsComponent } from './posts/posts.component';
 import { AuthGuard } from './helpers/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
+import { NonAuthGuard } from './helpers/nonAuth.guard';
 
 export const routes: Routes = [
     { path: '', component: IndexComponent },
     { path: 'template', component: TemplateFormComponent },
     { path: 'reactive', component: ReactiveFormComponent },
-    { path: 'login', component: LoginComponent },
+    { path: 'login', component: LoginComponent, canActivate: [NonAuthGuard] },
     { path: 'posts', component: PostsComponent, canActivate: [AuthGuard] }
 ];
